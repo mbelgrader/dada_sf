c5929ceaf9e64782bb1eb20c0ec5a87fdf900116
Maria Belgrader <mbelgrader5@gmail.com>
2016-05-07 20:50:39 -0700

    Fix images to lightbox

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
   // Adjust image size to thumbnails
   $("div.gallery img").width(300).height(300);
 
-
   $("img").click(function() {
-
     // Save clicked image src
     var image_src = $(this).attr("src");
     // Display backdrop and lightbox
@@ -13,13 +11,17 @@ $(document).ready(function(){
 
     // Add image to lightbox
     $("div.box").html('<img src="' + image_src + '" />');
-    // Center the lightbox on the screen
 
-    // var box_height = $("div.box").height();
-    // $("img.box").css("height", box_height);
     // Exit by clicking backdrop
     $("div.backdrop").click(function() {
       $(".box, .backdrop").hide();
     })
   })
+
+  // Arrows
+  //
+  // $("button#left_arrow").click(function() {
+  //   next_image =
+  // })
+
 });
