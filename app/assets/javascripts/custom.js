7c93b829805bd222e689c7d183799cfd2c41f183
Maria Belgrader <mbelgrader5@gmail.com>
2016-05-09 04:43:59 -0700

    Add social media icons

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  });
+
   // Adjust image size to thumbnails
   $("div.gallery img").width(300).height(300);
+  $("div.gallery img")
 
   $("img").click(function() {
     // Save clicked image src
     var image_src = $(this).attr("src");
-
+    var next_image = $(this).next().attr("src");
+    var prev_image = $(this).prev().attr("src");
     // Display backdrop and lightbox
     $(".backdrop").show();
     $(".box").show();
@@ -16,16 +16,20 @@ $(document).ready(function(){
     // Add image to lightbox
     $("div.box").html('<img src="' + image_src + '" />');
 
+    // Arrows
+    $("#right_arrow").click(function() {
+      $("div.box img").attr("src", next_image);
+      image_src = next_image;
+    })
+
+    $("#left_arrow").click(function() {
+      $("div.box img").attr("src", prev_image);
+    })
+
     // Exit by clicking backdrop
-    $("div.backdrop").click(function() {
+    $("#close_lightbox").click(function() {
       $(".box, .backdrop").hide();
     })
   })
-
-  // Arrows
-  //
-  // $("button#left_arrow").click(function() {
-  //   next_image =
-  // })
 
 });
